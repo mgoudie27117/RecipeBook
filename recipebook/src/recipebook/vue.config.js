71fb282b4253475f851d1eeac2985cd27e9d4469
Michael Goudie <mgoudie27117@users.noreply.github.com>
2021-08-08 11:44:17 -0500

    Production config change
    Change for Vue config for production server.

--- recipebook/src/recipebook/vue.config.js
+++ recipebook/src/recipebook/vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
         port: 80,
         proxy: {
             '/api': {
-                target: 'http://localhost:8080',
+                target: 'http://ec2-3-134-79-22.us-east-2.compute.amazonaws.com:8080',
                 ws: true,
                 changeOrigin: true
             }
